e253c0c018ad1e9713f4b60a3f3400312bef2276
zzy <17612723803@163.com>
2019-05-18 18:53:38 +0800

    删除无用包

--- xc-service-manage-cms/src/main/java/com/xczx/cms/web/controller/CmsPageController.java
+++ xc-service-manage-cms/src/main/java/com/xczx/cms/web/controller/CmsPageController.java
@@ -2,18 +2,12 @@ package com.xczx.cms.web.controller;
 
 import com.xczx.cms.service.PageService;
 import com.xuecheng.api.cms.CmsPageControllerApi;
-import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
-import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
-import com.xuecheng.framework.model.response.QueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/cms/page")
@@ -32,15 +26,6 @@ public class CmsPageController implements CmsPageControllerApi {
     @Override
     @RequestMapping("/list/{page}/{size}")
     public QueryResponseResult findList(@PathVariable int page,@PathVariable int size, QueryPageRequest queryPageRequest) {
-//        QueryResult<Object> queryResult = new QueryResult<>();
-//        queryResult.setTotal(2);
-//        //静态数据列表
-//        List list = new ArrayList();
-//        CmsPage cmsPage = new CmsPage();
-//        cmsPage.setPageName("测试页面");
-//        list.add(cmsPage);
-//        queryResult.setList(list);
-//        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
         return pageService.findList(page,size,queryPageRequest);
     }
 }
